9aff4c85457cd6b5a7a734c292dea3fc8857f444
Hedriw <32296768+Hedriw@users.noreply.github.com>
2017-09-27 13:08:06 +0200

    ResearchController fini

--- controller/ResearchController.php
+++ controller/ResearchController.php
@@ -9,7 +9,14 @@ class ResearchController extends Controller{
 													where LOWER(t_e_acteur_act.act_nom) like",
 							"realisateur"=>"Select DISTINCT t_e_video_vid.vid_id from t_e_video_vid 
 												join t_e_realisateur_rea on (t_e_video_vid.rea_id=t_e_realisateur_rea.rea_id)
-												where LOWER(t_e_realisateur_rea.rea_nom) like"
+												where LOWER(t_e_realisateur_rea.rea_nom) like",
+							"realisateurvide"=>"Select t_e_video_vid.vid_id from t_e_video_vid 
+												join t_e_realisateur_rea on (t_e_video_vid.rea_id=t_e_realisateur_rea.rea_id) 
+												order by rea_nom",
+							"acteurvide"=>"Select t_e_video_vid.vid_id from t_e_video_vid
+											join t_j_acteurvideo_acv on (t_e_video_vid.vid_id =t_j_acteurvideo_acv.vid_id)
+											join t_e_acteur_act on (t_j_acteurvideo_acv.act_id = t_e_acteur_act.act_id)
+											order by t_e_acteur_act.act_nom"
 	);
 		
 	}
@@ -18,20 +25,32 @@ class ResearchController extends Controller{
 	public function index(){
 		if (isset(parameters()["typesearch"]) && isset(parameters()["keyword"]))
 		{
-			if(!empty(parameters()["keyword"]))
+			$sql="";
+			if(!empty(parameters()["keyword"])&& parameters()["keyword"]!="")
 			{
 				$sql=$this->requests[parameters()["typesearch"]]." lower('".parameters()["keyword"]."%')";
-				$st= db()->prepare($sql);
-				$st->execute();
-				$row = $st->fetch(PDO::FETCH_ASSOC);
-				if(!empty($row))
+			}
+			else
+			{
+				$field =parameters()["typesearch"]."vide";
+				$sql=$this->requests[$field];
+			}
+			$st= db()->prepare($sql);
+			$st->execute();
+			$row = $st->fetch(PDO::FETCH_ASSOC);
+			if(!empty($row))
+			{
+				$videos=array();
+				foreach($row as $field=>$value)
 				{
-					foreach($row as $field=>$value)
-					{
-						$videos[$value]=new Video($value);
-					}
-					$this->render("index",$videos);
+					$videos[$value]=new Video($value);
 				}
+				$this->render("index",$videos);
+			}
+			else
+			{
+				$error = "Aucune vidéo à été trouvé";
+				$this->render("index",$error);
 			}
 		}
 		else if(!isset(parameters()["typesearch"]))

79b29f6c48879682c77b6f18791db0b3c407d0fe
Hedriw <32296768+Hedriw@users.noreply.github.com>
2017-09-27 08:35:16 +0200

    New DB

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
-$db = new PDO("pgsql:host=localhost port=5433;dbname=duvety","duvety","wHgGzY");
-$result=$db->exec('SET search_path to duvetyscrum');
+$db = new PDO("pgsql:host=localhost port=5433;dbname=g246","g246","ZXQewP");
+$result=$db->exec('SET search_path to g246scrum');
 
 function db() { global $db; return $db; }
 

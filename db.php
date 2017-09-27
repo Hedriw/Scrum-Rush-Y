3acc30f795703036c7a69e81d0f8277db3d9c185
Hedriw <32296768+Hedriw@users.noreply.github.com>
2017-09-27 08:37:17 +0200

    DB fonctionnel

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO("pgsql:host=localhost port=5433;dbname=g246","g246","ZXQewP");
+$db = new PDO("pgsql:host=localhost;dbname=g246","g246","ZXQewP");
 $result=$db->exec('SET search_path to g246scrum');
 
 function db() { global $db; return $db; }
